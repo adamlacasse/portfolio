35ea6fdc04a12fed25044a1e4069e727f558abf9
ADAM LACASSE <adam.lacasse@libertymutual.com>
2021-02-02 18:32:40 -0500

    styling; collapsible bio section

--- react-refactor/src/components/Header.js
+++ react-refactor/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../styles/Header.scss';
-import reduxImage from '../images/react-redux.png'
+import awlImage from '../images/awl.jpg'
 
 export default () => {
   return (
     <header>
-        <h1>Hi, I'm Adam.</h1>
-        <img src={reduxImage}></img>
+        <h1>Hi! I'm Adam, a Full Stack Software Engineer.</h1>
+        <img src={awlImage} alt="Adam LaCasse"></img>
     </header>
   );
 }

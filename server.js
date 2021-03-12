1505045e57e504e39ad6f991bc940788b44b4eb6
ADAM LACASSE <adam.lacasse@libertymutual.com>
2021-03-12 16:52:42 -0500

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, 'build');
-app.use(express.static(publicPath));
+app.use(express.static(`${__dirname}/build`));
 
-
-app.listen(port, () => {
-    if (port === 3000) {
-        console.log(`Server is up at http://localhost:${port}`);
-    } else {
-        console.log(`Server is up on port ${port}`);
-    }
-});
+app.listen(
+    port, 
+    () => port === 3000 ? console.log(`Server is up at http://localhost:${port}`) : console.log(`Server is up on port ${port}`)
+);

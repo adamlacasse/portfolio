8bc3853e2cba1516f3782b43ea46adae49ecc8a7
Adam LaCasse <36715261+adamlacasse@users.noreply.github.com>
2023-06-15 10:58:16 -0400

    Redirecting to Vercel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+window.location = 'https://portfolio-2023-bay.vercel.app/';

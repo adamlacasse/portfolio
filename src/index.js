f701cbac993aa01e270e4a9c9ce3158a09e118c2
Adam LaCasse <36715261+adamlacasse@users.noreply.github.com>
2023-06-15 11:11:55 -0400

    Revert index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-window.location = 'https://portfolio-2023-bay.vercel.app/';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
